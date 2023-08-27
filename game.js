7d62968c1e4bd55853be2f8b369c59d0ba4a0736
meaggy <glowackm@mcmaster.ca>
2023-08-27 14:52:53 -0400

    Complete game reset on wrong answer

--- game.js
+++ game.js
@@ -1,25 +1,22 @@
-let gamePattern = [];
-let userPattern = [];
 let buttons = ["red", "blue", "green", "yellow"];
 let initalize = true;
-let level = 0;
 
-
-// let clickCount = 0;
 
 // Begin game
 $(document).keydown((event) => {
-    if (initalize === true ){        
-        // $("h1").text("Level 0");
+    if (initalize === true ){    
+        gamePattern = [];
+        userPattern = [];
+        level = 0;
+        initalize = true;    
+        $("#level-title").text("Level 0");
         nextSequence();
         initalize = false;
     };
-
 });
 
-// Helpers 
+// Helpers -------------------------------------------------------------
 function animatePress(color){
-    // console.log(color);
     $(`#${color}`).addClass("pressed");
     setTimeout( () => {$(`#${color}`).removeClass("pressed")}, 100);   
 };
@@ -28,21 +25,14 @@ function playSound(name) {
     let audio = new Audio(`sounds/${name}.mp3`);
     audio.play(); 
 };
-//
-
-function checkAnswer(currentLevel) {
-    console.log("checking", currentLevel);
-    console.log(userPattern, "==>", gamePattern);
-    if (userPattern[currentLevel] === gamePattern[currentLevel]){
-        // console.log("correct!");
+///---------------------------------------------------------------------
+function checkAnswer(index){
+    if (userPattern[index] === gamePattern[index]){
         return true;
     }
     else {
-        console.log("wrong!"); 
-
         return false;
     };
-
 };
 
 function nextSequence() {
@@ -51,16 +41,9 @@ function nextSequence() {
     let randomColor = buttons[randomNumber];
     animatePress(randomColor);
     playSound(randomColor);
-    console.log("Game pattern color ===>", randomColor);
     gamePattern.push(randomColor);
-
+    level++;
 };
-
-// function resetTrackers(){
-//     // checkAnswer = false; // reset
-//     userPattern = null; // reset    
-// };
-// levels are wrong! checkig the wrong index
 
 // Main functionality
 $(".btn").click((event) => {
@@ -68,20 +51,18 @@ $(".btn").click((event) => {
     animatePress(clickedColor);
     playSound(clickedColor);
     userPattern.push(clickedColor);
-    //if check answer and pattern length
-    if (checkAnswer(level)) {
-        // debugger;
-        
-        if (userPattern.length === gamePattern.length){
-             console.log("correct, next sequence ");
-
-             setTimeout(nextSequence, 1000);
-
-             userPattern = []; // reset
-        };
-       
-        
-    };
-    // else {console.log("game over!")}
-   }
+    let checkHelper = userPattern.length -1;
+    if (checkAnswer(checkHelper)) { 
+        if (userPattern.length === gamePattern.length) {
+            setTimeout(nextSequence, 1000);
+            userPattern = []; // reset      
+        } 
+    } else {
+            playSound("wrong");
+            $("body").addClass("game-over");
+            setTimeout( () => {$("body").removeClass("game-over")}, 200);
+            $("#level-title").text("Game Over! Press any key to restart");
+            initalize = true;
+        };     
+    }
 );
